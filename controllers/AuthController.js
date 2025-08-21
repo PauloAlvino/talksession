5afbf3df911ce2f431f6eb4f10b33912f8304421
Pauloricardo1808 <pr243140@gmail.com>
2025-08-21 00:27:02 -0300

    feat: finalizar validação e logica e criação de sessao do usuario

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,5 +1,5 @@
 const User = require("../models/User");
-
+const bcrypt = require("bcryptjs");
 module.exports = class AuthController {
   static login(req, res) {
     res.render("auth/login");
@@ -8,12 +8,40 @@ module.exports = class AuthController {
     res.render("auth/register");
   }
   static async postUser(req, res) {
-    const { name, email, password } = req.body;
+    const emailRegex =
+      /^([a-z]){1,}([a-z0-9._-]){1,}([@]){1}([a-z]){2,}([.]){1}([a-z]){2,}([.]?){1}([a-z]?){2,}$/i;
+    const { name, email, password, confirm } = req.body;
+    if (!name || !email || !password || !confirm) {
+      req.flash("message", "Todos os Campos São Obrigatórios");
+      res.render("auth/register");
+      return;
+    }
+    if (!emailRegex.test(email)) {
+      req.flash("message", "Email Inválido, Tente Novamente");
+      res.render("auth/register");
+      return;
+    }
+    if (password != confirm) {
+      req.flash("message", "As Senhas Não Conferem, Tente Novamente");
+      res.render("auth/register");
+      return;
+    }
 
+    const userExists = await User.findOne({ where: { email: email } });
+    if (userExists) {
+      req.flash("message", "Usuário Já Existe, Tente Novamente");
+      res.render("auth/register");
+      return;
+    }
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(password, salt);
     try {
-      await User.create({ name, email, password });
-
-      res.redirect("/login");
+      const user = await User.create({ name, email, password: hashedPassword });
+      req.session.userId = user.id;
+      req.flash("sucess", "Usuário Registrado Com Sucesso!");
+      req.session.save(function () {
+        res.redirect("/");
+      });
     } catch (err) {
       console.error(err);
       res.status(500).send("Erro ao registrar usuário");
